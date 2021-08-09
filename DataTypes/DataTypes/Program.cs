d20991bad726601d13b4ed73693c77ba3ac4fff5
Hannah <HannahMarie-Wilson@live.nmit.ac.nz>
2021-08-09 14:08:05 +1200

    Stuff and thing

---
+++ DataTypes/DataTypes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 75;
+
+            char c = 'k';
+
+            string s = "KLM";
+
+            Int16 n16 = 123;
+            UInt16 nTn16 = 123;
+            Int32 n32 = 234;
+            UInt32 nI32 = 1223;
+            Int64 n64 = 456;
+            UInt64 nu64 = 3434;
+        }
+    }
+}
